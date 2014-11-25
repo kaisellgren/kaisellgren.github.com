5d1bb752e5e07f17ce89254e81ef5fccd0d322f7
Kai Sellgren <kaisellgren@gmail.com>
2014-11-25 19:50:29 +0000

    Update documentation

---
+++ doc/implementors/std/error/trait.FromError.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rust_git'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/std/error/trait.FromError.html' title='std::error::FromError'>FromError</a>&lt;<a class='struct' href='http://doc.rust-lang.org/nightly/std/io/struct.IoError.html' title='std::io::IoError'>IoError</a>&gt; for <a class='enum' href='rust_git/error/enum.GitError.html' title='rust_git::error::GitError'>GitError</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
