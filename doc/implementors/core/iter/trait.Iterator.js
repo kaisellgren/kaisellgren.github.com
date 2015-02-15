2beeac19e123c04a1c003bd771da0ae49b384d68
Kai Sellgren <kaisellgren@gmail.com>
2015-02-15 15:47:10 +0200

    Update documentation

---
+++ doc/implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['comp_sci'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, A&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a> for <a class='struct' href='comp_sci/data_structures/doubly_linked_list/struct.Iter.html' title='comp_sci::data_structures::doubly_linked_list::Iter'>Iter</a>&lt;'a, A&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
